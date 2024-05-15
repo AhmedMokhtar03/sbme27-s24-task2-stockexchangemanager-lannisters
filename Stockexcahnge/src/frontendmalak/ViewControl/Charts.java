ad81f2373fe9b4ce34ca4d9cb19be5501024750c
YasmeenBadr <165474689+YasmeenBadr@users.noreply.github.com>
2024-05-16 01:37:40 +0300

    to menna (select company)

--- Stockexcahnge/src/frontendmalak/ViewControl/Charts.java
+++ Stockexcahnge/src/frontendmalak/ViewControl/Charts.java
@@ -1,15 +1,81 @@
 package frontendmalak.ViewControl;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import static frontendmalak.HelloApplication.stg;
 
-public class Charts {
+public class Charts implements Initializable {
+     private double x = 0,y = 0;
+
+     @FXML
+     private LineChart<?, ?> lineChart;
+
+     @FXML
+     private AnchorPane sideBar;
+
+     private Stage stage;
+
+
+
+     public void initialize() {
+          sideBar.setOnMousePressed(mouseEvent -> {
+               x = mouseEvent.getSceneX();
+               y = mouseEvent.getSceneY();
+          });
+
+          sideBar.setOnMouseDragged(mouseEvent -> {
+               stage.setX(mouseEvent.getScreenX() - x);
+               stage.setY(mouseEvent.getScreenY() - y);
+          });
+
+          lineChart.getXAxis().setLabel("XAxis");
+          lineChart.getYAxis().setLabel("YAxis");
+
+          XYChart.Series series1 = new XYChart.Series();
+
+          series1.getData().add(new XYChart.Data("1",5));
+          series1.getData().add(new XYChart.Data("2",4));
+          series1.getData().add(new XYChart.Data("3",6));
+          series1.getData().add(new XYChart.Data("5",3));
+          series1.getData().add(new XYChart.Data("9",10));
+
+          XYChart.Series series2 = new XYChart.Series();
+
+          series2.getData().add(new XYChart.Data("1",2));
+          series2.getData().add(new XYChart.Data("3",2));
+          series2.getData().add(new XYChart.Data("4",5));
+
+          XYChart.Series series3 = new XYChart.Series();
+
+          series3.getData().add(new XYChart.Data("1",1));
+          series3.getData().add(new XYChart.Data("2",4));
+          series3.getData().add(new XYChart.Data("4",9));
+
+          lineChart.getData().addAll(series1,series2,series3);
+     }
+
+     public void setStage(Stage stage){
+          this.stage = stage;
+     }
+
+     @FXML
+     void closeProgram(ActionEvent event) {
+          stage.close();
+     }
+
      private ActionEvent event;
 
      public void Back3(ActionEvent event) throws IOException {
@@ -19,6 +85,11 @@ public class Charts {
           Scene scene = new Scene(root);
           stg.setScene(scene);
           stg.show();
+
+     }
+
+     @Override
+     public void initialize(URL url, ResourceBundle resourceBundle) {
 
      }
 }
--- Stockexcahnge/src/frontendmalak/ViewControl/Selectcompany.java
+++ Stockexcahnge/src/frontendmalak/ViewControl/Selectcompany.java
@@ -1,4 +1,21 @@
 package frontendmalak.ViewControl;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class Selectcompany {
-}
+    @FXML
+    private ChoiceBox<String> aapl;
+    @FXML
+    private ChoiceBox<String> amzn;
+    public void initialize() {
+
+        ObservableList<String> select = FXCollections.observableArrayList("subscribe", "unsubscribe");
+        aapl.setItems(select);
+        aapl.setValue("subscribe");
+        ObservableList<String> select1 = FXCollections.observableArrayList("subscribe", "unsubscribe");
+        amzn.setItems(select);
+        amzn.setValue("subscribe");
+}}
