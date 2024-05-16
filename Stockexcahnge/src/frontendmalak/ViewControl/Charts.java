0e5cfdf0bcc78baba13a99825da41de6a2134916
YasmeenBadr <165474689+YasmeenBadr@users.noreply.github.com>
2024-05-17 02:08:35 +0300

    line charts ready

--- Stockexcahnge/src/frontendmalak/ViewControl/Charts.java
+++ Stockexcahnge/src/frontendmalak/ViewControl/Charts.java
@@ -6,7 +6,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -18,69 +20,59 @@ import java.util.ResourceBundle;
 import static frontendmalak.HelloApplication.primaryStage;
 
 public class Charts implements Initializable {
-     private double x = 0,y = 0;
+
 
      @FXML
-     private LineChart<?, ?> lineChart;
+  private LineChart<String, Double> lineChart;
 
      @FXML
-     private AnchorPane sideBar;
 
-     private Stage stage;
+     private CategoryAxis xAxis;
+    @FXML
+    private NumberAxis yAxis;
 
 
 
-     public void initialize() {
-          sideBar.setOnMousePressed(mouseEvent -> {
-               x = mouseEvent.getSceneX();
-               y = mouseEvent.getSceneY();
-          });
+    @FXML
 
-          sideBar.setOnMouseDragged(mouseEvent -> {
-               stage.setX(mouseEvent.getScreenX() - x);
-               stage.setY(mouseEvent.getScreenY() - y);
-          });
 
-          lineChart.getXAxis().setLabel("XAxis");
-          lineChart.getYAxis().setLabel("YAxis");
 
-          XYChart.Series series1 = new XYChart.Series();
 
-          series1.getData().add(new XYChart.Data("1",5));
-          series1.getData().add(new XYChart.Data("2",4));
-          series1.getData().add(new XYChart.Data("3",6));
-          series1.getData().add(new XYChart.Data("5",3));
-          series1.getData().add(new XYChart.Data("9",10));
 
-          XYChart.Series series2 = new XYChart.Series();
+     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-          series2.getData().add(new XYChart.Data("1",2));
-          series2.getData().add(new XYChart.Data("3",2));
-          series2.getData().add(new XYChart.Data("4",5));
 
-          XYChart.Series series3 = new XYChart.Series();
 
-          series3.getData().add(new XYChart.Data("1",1));
-          series3.getData().add(new XYChart.Data("2",4));
-          series3.getData().add(new XYChart.Data("4",9));
 
-          lineChart.getData().addAll(series1,series2,series3);
+          XYChart.Series<String, Double> series1 = new XYChart.Series<>();
+
+               series1.setName("High");
+          series1.getData().add(new XYChart.Data("Jan",100.0));
+          series1.getData().add(new XYChart.Data("Feb",40.0));
+          series1.getData().add(new XYChart.Data("Mar",70.0));
+          series1.getData().add(new XYChart.Data("Aprl",60.0));
+          series1.getData().add(new XYChart.Data("May",50.0));
+
+        XYChart.Series<String, Double> series2 = new XYChart.Series<>();
+
+
+        series2.getData().add(new XYChart.Data("Jan",10.0));
+        series2.getData().add(new XYChart.Data("Feb",60.0));
+        series2.getData().add(new XYChart.Data("Mar",30.0));
+        series2.getData().add(new XYChart.Data("Aprl",120.0));
+        series2.getData().add(new XYChart.Data("May",100.0));
+
+
+
+
+
+
+          lineChart.getData().addAll(series1,series2);
      }
 
-     public void setStage(Stage stage){
-          this.stage = stage;
-     }
 
-     @FXML
-     void closeProgram(ActionEvent event) {
-          stage.close();
-     }
-
-     private ActionEvent event;
-
-     public void Back3(ActionEvent event) throws IOException {
-          this.event = event;
-          FXMLLoader loader = new FXMLLoader(getClass().getResource("/frontendmalak/View/UserView.fxml"));
+    public void Back3(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/frontendmalak/View/UserView.fxml"));
           Parent root = loader.load();
           Scene scene = new Scene(root);
           primaryStage.setScene(scene);
@@ -88,16 +80,12 @@ public class Charts implements Initializable {
 
      }
      public void Gopremium(ActionEvent event) throws IOException {
-          this.event = event;
-          FXMLLoader loader = new FXMLLoader(getClass().getResource("/frontendmalak/View/premiumsubscribtion.fxml"));
+         FXMLLoader loader = new FXMLLoader(getClass().getResource("/frontendmalak/View/premiumsubscribtion.fxml"));
           Parent root = loader.load();
           Scene scene = new Scene(root);
           primaryStage.setScene(scene);
           primaryStage.show();
      }
 
-     @Override
-     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-     }
 }
